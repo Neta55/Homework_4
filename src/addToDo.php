f242fd48a454da0e931261a7c6c913d0e4e8eeea
Ineta Priga <ineta.priga@gmail.com>
2019-12-11 16:16:56 +0200

    areas sataisitas

--- src/addToDo.php
+++ src/addToDo.php
@@ -1,6 +1,8 @@
 <?php
+
 if (!$_SESSION['username']) {
     return;
+    
 }
 ?>
 
--- src/printToDo.php
+++ src/printToDo.php
@@ -35,21 +35,21 @@ foreach ($allRows as $row) {
     } else {
         $special = "Done-null";
     }
+   
     echo "<div class='todoList'>";
-    echo "<p class='todoText todo_task' $special>" .$row['todotasks']. "</p>";
+    // echo "<div class='int-cont'>";
+    echo "<form action='updateToDo.php' method='post' class='int-cont'>";
+    echo "<div class='todo_task'><p class='todoText $special'>" .$row['todotasks']. "</p></div>";
     
-
-    
-    echo "<form action='updateToDo.php' method='post'>";
     foreach ($row as $key => $value) {
     switch ($key) {
             case 'todoDone':
                 //set checked to show if we have a set value
                 $checked = $value ? "checked" : "";
-                echo "<input type='checkbox' class='check-btn todo_check' name='$key' value='$key' $checked></input>";
+                echo "<div class='todo_check'><input type='checkbox' class='check-btn' name='$key' value='$key' $checked></input></div>";
                 break;
             case 'todotasks':
-                echo "<div class='dropdown todo_edit'><div class='edit-btn'></div><div class='dropdown-content'><input class='edit-task' name='$key' value='$value'></input></div></div>";
+                echo "<div class='dropdown todo_edit'><div class='edit-btn'></div><div class='dropdown-content'><input class='edit-task' name='$key' value='$value'></div></div>";
                 break;
 
         }
@@ -57,13 +57,13 @@ foreach ($allRows as $row) {
     
 
     
-    echo "<button name='update' class='update-btn todo_update' value='" . $row['id'] . "'></button>";
+    echo "<div class='todo_update'><button name='update' class='update-btn ' value='" . $row['id'] . "'></button></div>";
     echo "</form>";
     // echo "</div>";
 
     
     echo "<form action='deleteToDo.php' method='post'>";
-    echo "<button name='delete-btn' class='delete-btn todo_delete' value='" . $row['id'] . "'></button>";
+    echo "<div class='todo_delete'><button name='delete-btn' class='delete-btn' value='" . $row['id'] . "'></button></div>";
     echo "</form>";
     echo "</div>";
 }
